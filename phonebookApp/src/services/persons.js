61c0f683bcefb6c10e24d08616defb99f0fcdfae
Andrespcom <andres.perez.comesana@gmail.com>
2025-05-23 13:36:28 +0200

    correccion baseUrl

--- phonebookApp/src/services/persons.js
+++ phonebookApp/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'https://phonebook-backend-kwgg.onrender.com'
+const baseUrl = 'https://phonebook-backend-kwgg.onrender.com/api/persons'
 
 const getAll = () => {
   return axios.get(baseUrl).then(response => response.data)
